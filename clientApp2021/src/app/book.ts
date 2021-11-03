2bc0db07a70e7973010a12e93bb46f1741bdb08f
Jack Malone <S00199917@mail.itsligo.ie>
2021-11-03 17:09:53 +0000

    Finished all exercises

--- clientApp2021/src/app/book.ts
+++ clientApp2021/src/app/book.ts
@@ -1,2 +1,12 @@
 export interface Book {
+    _id : string,
+    title: string,
+    year_written: number,
+    edition: string,
+    price: number,
+    author: {
+        name: string,
+        nationality : string
+    }
+    tags: string[];
 }
--- clientApp2021/src/app/book/book.component.ts
+++ clientApp2021/src/app/book/book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
+
 
 @Component({
   selector: 'app-book',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+  books: Book[] = [];
+  message: any;
 
-  constructor() { }
+  constructor(private bookService : BookService) { }
 
   ngOnInit(): void {
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.books = value,
+      complete: () => console.log('book service finished'),
+      error: (message) => this.message = message
+    })
   }
 
 }
